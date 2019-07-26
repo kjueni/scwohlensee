efe930e06cb4ad5395c1681e83d45182852b40d1
Kevin Jüni <kevin.jueni@gmail.com>
2019-07-26 23:41:17 +0200

    Working version with login

--- src/Clubster/Bundle/CoreBundle/ClubsterCoreBundle.php
+++ src/Clubster/Bundle/CoreBundle/ClubsterCoreBundle.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 
 namespace Clubster\Bundle\CoreBundle;
 
+use Clubster\Bundle\CoreBundle\CompilerPass\CollectFixturesPass;
 use Sylius\Bundle\ResourceBundle\AbstractResourceBundle;
 use Sylius\Bundle\ResourceBundle\ResourceBundleInterface;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class ClubsterCoreBundle extends AbstractResourceBundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new CollectFixturesPass());
+    }
+
     /**
      * Configure format of mapping files.
      *
