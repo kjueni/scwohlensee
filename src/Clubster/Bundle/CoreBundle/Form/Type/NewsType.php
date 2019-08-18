41f7cd36b1399d995f5bc201c44a7a8e09df75c3
Kevin Jüni <kevin.jueni+1@gmail.com>
2019-08-18 16:56:03 +0200

    Added custom form for news

--- src/Clubster/Bundle/CoreBundle/Form/Type/NewsType.php
+++ src/Clubster/Bundle/CoreBundle/Form/Type/NewsType.php
@@ -7,9 +7,9 @@ namespace Clubster\Bundle\CoreBundle\Form\Type;
 use Clubster\Component\Core\Model\Team;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Clubster\Bundle\PlayerBundle\Form\Type\PlayerType as BasePlayerType;
+use Clubster\Bundle\NewsBundle\Form\Type\NewsType as BaseNewsType;
 
-class PlayerType extends BasePlayerType
+class NewsType extends BaseNewsType
 {
     /**
      * @param FormBuilderInterface $builder
@@ -18,11 +18,12 @@ class PlayerType extends BasePlayerType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
-
+        
         $builder
-            ->add('team', EntityType::class, [
+            ->add('teams', EntityType::class, [
                 'class' => Team::class,
                 'choice_label' => 'name',
+                'multiple' => true,
             ]);
     }
 }
